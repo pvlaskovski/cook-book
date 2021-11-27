2f628e384e8fe763e037fbc4067ab2a5ba5c054a
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-11-27 22:12:07 +0100

    Set up StyledEngineProvider to override MUI stylesheets

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import {Toolbar, AppBar, Button} from '@mui/material';
 import { Link } from "react-router-dom";
-
-
+import './Header.css';
 
 function Header() {
 
@@ -11,7 +10,7 @@ function Header() {
                 <Toolbar>
                     <h1>Put logo here</h1>
 
-                    <Button color="inherit" component={Link} to="/register">Register</Button>
+                    <Button className="menuButton" color="inherit" component={Link} to="/register">Register</Button>
                     <Button color="inherit" component={Link} to="/login">Login</Button>
                     <Button color="inherit" component={Link} to="/logout">Logout</Button>
                     <Button color="inherit" component={Link} to="/recepies">Recepies</Button>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from '@mui/material/styles';
+
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <StyledEngineProvider injectFirst>
+                <App />
+            </StyledEngineProvider>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
