b279ed8d10edadf90601346f574bf91335fd5e04
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-11-29 19:23:14 +0100

    add Rating and update Recipe Details style

---
+++ src/components/CustomRating/CustomRating.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+import StarIcon from '@mui/icons-material/Star';
+
+import './CustomRating.css';
+
+
+const labels = {
+    1: 'Average',
+    2: 'Above Average',
+    3: 'Good',
+    4: 'Very good',
+    5: 'Amazing',
+};
+
+function CustomRating() {
+    const [value, setValue] = React.useState(2);
+    const [hover, setHover] = React.useState(-1);
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',  
+            }}
+        >
+            <Rating
+                name="hover-feedback"
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                onChangeActive={(event, newHover) => {
+                    setHover(newHover);
+                }}
+                emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+            />
+            {value !== null && (
+                <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+            )}
+        </Box>
+    );
+}
+
+export default CustomRating;
--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -1,35 +1,49 @@
 import { Box, width } from "@mui/system";
 import { Container, Paper } from "@mui/material";
 
+import './RecipeDetails.css';
+import CustomRating from "../CustomRating/CustomRating";
+
 function RecipeDetails() {
     return (
-        <>
-            <Paper>
-                <Container>
-                    <Box>
-                        <p>Ingredients</p>
-                        <ul>
-                            <li>500 gr beef</li>
-                            <li>2 tablespoons Dijon mustard (optional)</li>
-                            <li>1 large egg (free-range)</li>
-                            <li>1 round lettuce</li>
-                            <li>1 beef tomato</li>
-                        </ul>
-                    </Box>
+
+        <Paper className="paper" >
+            <h2>Name of the recepie</h2>
+            <span>Author and date and time</span>
+
+            <Container className="container">
+
+                <Box className="containerSection">
+                    <p>Ingredients</p>
+                    <ul>
+                        <li>500 gr beef</li>
+                        <li>2 tablespoons Dijon mustard (optional)</li>
+                        <li>1 large egg (free-range)</li>
+                        <li>1 round lettuce</li>
+                        <li>1 beef tomato</li>
+                    </ul>
+                </Box>
 
 
-                    <Box>
-                        <ol>
-                            <li>Using a bowl, mix the minced beef and mustard.</li>
-                            <li>Crack the egg into the bowl, then add a good pinch of sea salt and black pepper.</li>
-                            <li>With clean hands, scrunch and mix everything up well. Divide into 6 and pat and mould each piece into a roundish shape, roughly 2cm thick.</li>
-                            <li>Place the burgers onto a plate, drizzle with oil, then cover and place in the fridge until needed – this will help to firm them up.</li>
-                            <li>To cook the burgers, preheat a large griddle or frying pan for 4 minutes on a high heat, then turn the heat down to medium.</li>
-                        </ol>
-                    </Box>
-                </Container>
-            </Paper>
-        </>
+                <Box className="containerSection">
+                    <p>Steps</p>
+                    <ol>
+                        <li>Using a bowl, mix the minced beef and mustard.</li>
+                        <li>Crack the egg into the bowl, then add a good pinch of sea salt and black pepper.</li>
+                        <li>With clean hands, scrunch and mix everything up well. Divide into 6 and pat and mould each piece into a roundish shape, roughly 2cm thick.</li>
+                        <li>Place the burgers onto a plate, drizzle with oil, then cover and place in the fridge until needed – this will help to firm them up.</li>
+                        <li>To cook the burgers, preheat a large griddle or frying pan for 4 minutes on a high heat, then turn the heat down to medium.</li>
+                    </ol>
+                </Box>
+
+            </Container>
+           
+      
+            <CustomRating />
+          
+            
+        </Paper>
+
     )
 }
 
