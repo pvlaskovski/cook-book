989f51a484192ea922b9c09ae73952e9162328f3
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-12-05 01:12:40 +0100

    add link to recipe details

--- src/components/RecepiesGrid/RecepiesGrid.js
+++ src/components/RecepiesGrid/RecepiesGrid.js
@@ -23,7 +23,7 @@ function RecepiesGrid() {
                 console.log(recipeDetails);
                 return(
                     <Grid item xs={12} sm={6} md={3} key={recipeId}>
-                        <RecipeCard className="recipeContainer"  recipe={recipeDetails} />
+                        <RecipeCard className="recipeContainer"  recipeId= {recipeId} recipe={recipeDetails} />
                     </Grid>
                 )
             })
--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,40 +1,39 @@
 import * as React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import { CardActionArea, Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography } from '@mui/material';
 
-function RecipeCard({recipe}) {
+function RecipeCard({recipeId, recipe}) {
 
     return (
         <Card>
-            <CardActionArea>
-                
-                <CardHeader
-                    avatar={
-                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            R
-                        </Avatar>
-                    }
+            <CardActionArea component={RouterLink} to={`recipe/${recipeId}`}>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                R
+                            </Avatar>
+                        }
 
-                    title={(recipe.recipeName)? recipe.recipeName : 'No name'}
-                    // title="Some Title"
-                    subheader="September 14, 2016"
-                />
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image={recipe.imgUrl}
-                    alt={recipe.recipeName}
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        {/* To add short description to the add recipe input */}
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
-                    </Typography>
-                </CardContent>
+                        title={(recipe.recipeName)? recipe.recipeName : 'No name'}
+                        subheader="September 14, 2016"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image={recipe.imgUrl}
+                        alt={recipe.recipeName}
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            {/* To add short description to the add recipe input */}
+                            This impressive paella is a perfect party dish and a fun meal to cook
+                            together with your guests. Add 1 cup of frozen peas along with the mussels,
+                            if you like.
+                        </Typography>
+                    </CardContent>     
             </CardActionArea>
 
             <CardActions disableSpacing>
