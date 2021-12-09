4d12786d07d82d0f6d42389e1132b1906bb95027
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-12-09 15:31:17 +0100

    Add dummy footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import SidebarFilters from './components/SideberFilters/SidebarFilters';
 import RecepiesGrid from './components/RecepiesGrid/RecepiesGrid';
 import RecipeDetails from './components/RecipeDetails/RecipeDetails';
 import AddRecipe from './components/AddRecipe/AddRecipe';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        {/* <RecepiesGrid/> */}
-        {/* <Register/> */}
-        {/* <Login/> */}
-        {/* <SidebarFilters/>
-        <RecipeDetails/> */}
+        <RecepiesGrid/>
+        <Register/>
+        <Login/>
+        <SidebarFilters/>
+        <RecipeDetails/>
         <AddRecipe/>
+        <Footer/>
     </div>
   );
 }
