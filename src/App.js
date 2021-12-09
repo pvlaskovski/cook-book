22efaba9a05ad859d69c1a50c9a187341d04e0bb
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-12-09 16:00:44 +0100

    Add main routes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import RecepiesGrid from './components/RecepiesGrid/RecepiesGrid';
 import RecipeDetails from './components/RecipeDetails/RecipeDetails';
 import AddRecipe from './components/AddRecipe/AddRecipe';
 import Footer from './components/Footer/Footer';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        <RecepiesGrid/>
-        <Register/>
-        <Login/>
-        <SidebarFilters/>
-        <RecipeDetails/>
-        <AddRecipe/>
+        
+        <Routes>
+            <Route path="/" element={<RecepiesGrid/>} />
+            <Route path="/recipes" element={<RecepiesGrid/>} />
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>} />
+            <Route path="/addRecipe" element={<AddRecipe/>} />
+            <Route path="/details/" element={<RecipeDetails/>} />
+        </Routes>
+
         <Footer/>
+        {/* <SidebarFilters/> */}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,8 @@ function Header() {
                     <Button className="menuButton" color="inherit" component={Link} to="/register">Register</Button>
                     <Button color="inherit" component={Link} to="/login">Login</Button>
                     <Button color="inherit" component={Link} to="/logout">Logout</Button>
-                    <Button color="inherit" component={Link} to="/recepies">Recepies</Button>
+                    <Button color="inherit" component={Link} to="/addRecipe">Add Recipe</Button>
+                    <Button color="inherit" component={Link} to="/recipes">Recipes</Button>
                 </Toolbar>
             </AppBar>
 
