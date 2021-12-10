3aafc418d1816f96cde95d6f1cbaf9bddc01c1af
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-12-10 16:43:42 +0100

    update to add recipe form

--- src/components/AddRecipe/AddRecipe.js
+++ src/components/AddRecipe/AddRecipe.js
@@ -1,4 +1,3 @@
-
 import { FormControl, InputLabel, Input, FormHelperText, Container, TextField, Select, MenuItem, Button, Box, Typography } from '@mui/material';
 import { useState } from 'react';
 
@@ -29,7 +28,6 @@ function AddRecipe() {
 
     const submitRecipe = function(){
         console.log("click submit");
-        // firebaseService.addRecipe();
         let formData = new FormData(document.querySelector('form'))
         // console.log(formData.get("recipeName"));
         // console.log(formData.get("recipeOverview"));
@@ -52,65 +50,65 @@ function AddRecipe() {
     return (
 
         // TODO: needs abstraction for the select to work with any input
-        <form>
-            <Container className="container">
-                <Typography>Add Recipe</Typography>
-                <TextField id="outlined-basic" label="Add a Recipe" variant="outlined" fullWidth name="recipeName" />
+        
+        <Container className="container" component="form">
+            <Typography>Add Recipe</Typography>
+            <TextField id="outlined-basic" label="Add a Recipe" variant="outlined" fullWidth name="recipeName"/>
 
-                <Typography>Short Overview</Typography>
-                <TextField label="Add overview" variant="outlined" fullWidth  multiline rows={2} name="recipeOverview"/>
+            <Typography>Short Overview</Typography>
+            <TextField label="Add overview" variant="outlined" fullWidth multiline rows={2} name="recipeOverview" />
 
 
-                <Container className="selectContainer">
-                    <FormControl className="select" >
-                        <InputLabel id="type">Type</InputLabel>
-                        <Select
-                            labelId="type"
-                            id="type-select"
-                            label="Type"
-                            value={type}
-                            onChange={handleTypeChange}
-                        >
-                            <MenuItem value="Soups">Soups</MenuItem>
-                            <MenuItem value="Deserts">Deserts</MenuItem>
-                            <MenuItem value="Main">Main</MenuItem>
-                        </Select>
-                    </FormControl>
+            <Container className="selectContainer">
+                <FormControl className="select" >
+                    <InputLabel id="type">Type</InputLabel>
+                    <Select
+                        labelId="type"
+                        id="type-select"
+                        label="Type"
+                        value={type}
+                        onChange={handleTypeChange}
+                    >
+                        <MenuItem value="Soups">Soups</MenuItem>
+                        <MenuItem value="Deserts">Deserts</MenuItem>
+                        <MenuItem value="Main">Main</MenuItem>
+                    </Select>
+                </FormControl>
 
-                    <FormControl className="select" >
-                        <InputLabel id="difficulty" >Difficulty</InputLabel>
-                        <Select
-                            labelId="difficulty"
-                            id="difficulty-select"
-                            label="Difficulty"
-                            value={difficulty}
-                            onChange={handleDifficultyChange}
-                        >
-                            <MenuItem value="Easy">Easy</MenuItem>
-                            <MenuItem value="Medium">Medium</MenuItem>
-                            <MenuItem value="Hard">Hard</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Container>
-
-                <Typography component="p">Add Ingredient</Typography>
-                <Container className="ingredientsContainer">
-                    <InsertItem getItems={getStateArrFromChild}/>
-                </Container>
-
-                <Typography component="p">Add Steps</Typography>
-                <Container className="methodContainer">
-                    <InsertStep getItems={getStateArrFromChild}/>
-                </Container>
-
-                <Container className="methodContainer">
-                    <TextField id="outlined-basic" label="Image Url" variant="outlined" fullWidth />
-                </Container>
-
-                <Button variant="contained" onClick={submitRecipe}>Submit</Button>
-
+                <FormControl className="select" >
+                    <InputLabel id="difficulty" >Difficulty</InputLabel>
+                    <Select
+                        labelId="difficulty"
+                        id="difficulty-select"
+                        label="Difficulty"
+                        value={difficulty}
+                        onChange={handleDifficultyChange}
+                    >
+                        <MenuItem value="Easy">Easy</MenuItem>
+                        <MenuItem value="Medium">Medium</MenuItem>
+                        <MenuItem value="Hard">Hard</MenuItem>
+                    </Select>
+                </FormControl>
             </Container>
-        </form>
+
+            <Typography component="p">Add Ingredient</Typography>
+            <Container className="ingredientsContainer">
+                <InsertItem getItems={getStateArrFromChild} />
+            </Container>
+
+            <Typography component="p">Add Steps</Typography>
+            <Container className="methodContainer">
+                <InsertStep getItems={getStateArrFromChild} />
+            </Container>
+
+            <Container className="methodContainer">
+                <TextField id="outlined-basic" label="Image Url" variant="outlined" fullWidth />
+            </Container>
+
+            <Button variant="contained" onClick={submitRecipe}>Submit</Button>
+
+        </Container>
+        
 
     )
 }
