5c771cead71addac751cf459712525814c88ec20
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-12-25 20:32:38 +0100

    cleanup routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import Header from './components/Header/Header';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-import Logout from './components/Logout/Logout';
-import RecipeDetails from './components/RecipeDetails/RecipeDetails';
-import AddRecipe from './components/AddRecipe/AddRecipe';
-import EditRecipe from './components/EditRecipe/EditRecipe';
-import Footer from './components/Footer/Footer';
-import { Route, Routes } from 'react-router';
 import { Toaster } from 'react-hot-toast';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import AllRoutes from './components/AllRoutes/AllRoutes';
+
 import { AuthProvider } from './contexts/AuthContext';
-import Homepage from './components/Pages/Homepage';
-
-import LoggedInUsersRoutes from './components/Common/LoggedInUsersRoutes';
-
 
 function App() {
     return (
@@ -21,23 +13,8 @@ function App() {
                 <div>
                     <Toaster />
                 </div>
-
                 <Header />
-
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/logout" element={<Logout />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="recipe/:recipeId" element={<RecipeDetails />} />
-
-                    <Route element={<LoggedInUsersRoutes />}>
-                        <Route path="/addRecipe" element={<AddRecipe />} />
-                        <Route path="recipe/:recipeId/edit" element={<EditRecipe />} />
-                    </Route>
-
-                </Routes>
-
+                <AllRoutes />
                 <Footer />
             </div>
         </AuthProvider>
---
+++ src/components/AllRoutes/AllRoutes.js
@@ -0,0 +1,29 @@
+
+import { Route, Routes } from 'react-router';
+
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Logout from '../Logout/Logout';
+import RecipeDetails from '../RecipeDetails/RecipeDetails';
+import AddRecipe from '../AddRecipe/AddRecipe';
+import EditRecipe from '../EditRecipe/EditRecipe';
+
+import Homepage from '../Pages/Homepage';
+import LoggedInUsersRoutes from '../Common/LoggedInUsersRoutes';
+
+export default function AllRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="recipe/:recipeId" element={<RecipeDetails />} />
+
+            <Route element={<LoggedInUsersRoutes />}>
+                <Route path="/addRecipe" element={<AddRecipe />} />
+                <Route path="recipe/:recipeId/edit" element={<EditRecipe />} />
+            </Route>
+        </Routes>
+    )
+}
