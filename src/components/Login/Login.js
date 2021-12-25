167295a691011628f853548f8d4e216a201bb1e0
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2021-12-25 20:44:39 +0100

    cleanup login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,13 @@
 import firebaseService from '../../services/firebase';
 
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import { Button, TextField, Grid, Box, Typography, Container } from '@mui/material';
+
+import toast from 'react-hot-toast';
 
 import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
 import { useAuthContext } from '../../contexts/AuthContext';
 
 export default function SignUp(props) {
-
     let navigate = useNavigate();
 
     const {login} = useAuthContext();
@@ -43,9 +36,8 @@ export default function SignUp(props) {
     return (
         <Container component="div" maxWidth="xs" >
             <Box >
-                <Avatar />
 
-                <Typography component="h1" variant="h5">Log in</Typography>
+                <Typography component="h1" variant="h5" align="center">Log in</Typography>
 
                 <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
                     <Grid container spacing={2}>
@@ -62,16 +54,8 @@ export default function SignUp(props) {
                     <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
                         Log in
                     </Button>
-
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            <Link href="/forgot" variant="body2">
-                                Forgot your password?
-                            </Link>
-                        </Grid>
-                    </Grid>
-
                 </Box>
+
             </Box>
         </Container>
     );
